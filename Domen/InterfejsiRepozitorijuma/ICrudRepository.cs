0fc35ed60f61bd26f7e7fe0ae941b95f9983e884
dejana šević <dejana.sevic24@gmail.com>
2025-10-02 20:01:38 +0200

    feat: add ICrud repository and IPersistenceContext

---
+++ Domen/InterfejsiRepozitorijuma/ICrudRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace MatchaLatteReviews.Domen.InterfejsiRepozitorijuma
+{
+    public interface ICrudRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Add(T entity);
+        T GetById(string id);
+        T Update(T entity);
+        void DeleteById(string id);
+        void SaveAll(IEnumerable<T> entities);
+    }
+}
