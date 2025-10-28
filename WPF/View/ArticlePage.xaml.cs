83cd8ce7e03033000fefae647abeb31c04e8c4bb
nadjalucic <lucic.sv50.2023@uns.ac.rs>
2025-10-28 20:51:59 +0100

    New files

---
+++ WPF/View/ArticlePage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MatchaLatteReviews.Domain.Model;
+using MatchaLatteReviews.WPF.ViewModel;
+using System.Windows;
+
+namespace MatchaLatteReviews.WPF.View
+{
+    public partial class ArticlePage : Window
+    {
+        public ArticlePage(Article article)
+        {
+            InitializeComponent();
+            DataContext = new ArticleViewModel(article);
+        }
+
+        private void OpenReviews_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is ArticleViewModel vm)
+            {
+                var win = new LoginPage();
+                //var win = new ReviewsWindow(vm.Id, vm.Title); // 
+                win.Owner = this;
+                win.ShowDialog();
+            }
+        }
+
+
+        private void Close_Click(object sender, RoutedEventArgs e) => Close();
+    }
+}
