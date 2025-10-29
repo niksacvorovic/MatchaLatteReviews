14262efd8f528b52b405f15e82cfb9b6badfdc7a
Vedran <bajic.sv10.2023@uns.ac.rs>
2025-10-29 22:34:50 +0100

    add: approving music

---
+++ WPF/View/ApproveMusicForm.xaml.cs
@@ -0,0 +1,30 @@
+﻿using MatchaLatteReviews.Domain.Model;
+using MatchaLatteReviews.WPF.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MatchaLatteReviews.WPF.View
+{
+    /// <summary>
+    /// Interaction logic for ApproveMusicForm.xaml
+    /// </summary>
+    public partial class ApproveMusicForm : Window
+    {
+        public ApproveMusicForm(Music music)
+        {
+            InitializeComponent();
+            DataContext = new ApproveMusicFormViewModel(this.Close, music);
+        }
+    }
+}
